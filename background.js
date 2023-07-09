3ced771b4d0fafb2dae54ee30334338f20eeeb8f
takahashikosuke <takahashikosuke@team-lab.com>
2023-07-10 00:23:08 +0900

    :wrench: construction commit

--- background.js
+++ background.js
@@ -1,12 +1,34 @@
-const sampleFunction = () => {
-  window.alert("sample");
-};
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  chrome.storage.local.clear();
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  console.log(tabId);
-  console.log(changeInfo);
-  console.log(tab);
+  chrome.storage.local.set({
+    domainList: {
+      list: [
+        {
+          url: "https://qiita.com/opportunities",
+          color: "red",
+          title: "本番",
+        },
+      ],
+    },
+  });
+
+  // ロードの完了を待つ
   if (changeInfo.status === "complete") {
+    const list = (await chrome.storage.local.get("domainList"))["domainList"];
+
+    const targetLists = list["list"];
+
+    // storageに対象のリストがない場合
+    if (!targetLists) return;
+
+    const isTargetSite = targetLists.some(
+      ({ url }) => tab.url.indexOf(url) > -1
+    );
+
+    // 対象サイトでない場合は何もしない
+    if (!isTargetSite) return;
+
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       files: ["main.js"],
--- main.js
+++ main.js
@@ -1,8 +1,26 @@
-const addBar = () => {
+const addBar = async () => {
+  const currentUrl = window.location.href;
+
+  const list = (await chrome.storage.local.get("domainList"))["domainList"];
+
+  const targetLists = list["list"];
+
+  // 対象サイトがない場合
+  if (!targetLists) return;
+
+  const currentEnv = targetLists.find(({ url }) => currentUrl.startsWith(url));
+
+  // 対象の環境がない場合
+  if (!currentEnv) return;
+
+  const text = currentEnv.title || "sampleです";
+  const color = currentEnv.color || "white";
+
   const body = document.getElementsByTagName("body");
   const ele = document.createElement("div");
-  ele.innerHTML = "sampleです";
-  ele.style.cssText = "width: 100%; height: 40px; position: sticky;";
+  ele.innerHTML = text;
+  ele.style.cssText = `width: 100%; height: 40px; position: sticky; background-color: ${color}`;
   body.item(0).prepend(ele);
 };
+
 addBar();
